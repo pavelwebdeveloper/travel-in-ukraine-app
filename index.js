dd522c9f619fc3b9ab7fb2a36ed1d2016136fdf1
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-28 11:34:38 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// index.js for travel-in-ukraine-app
+// load the things we need
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // index2 page
+  .get('/', (req, res) => res.render('pages/index2'))
+  // placesofinterest page
+  .get('/placesofinterest', (req, res) => res.render('pages/placesofinterest'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
