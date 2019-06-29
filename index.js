5869a04c698db25a1c934db9ce0cd81ccddfd06c
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-30 02:40:28 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js for travel-in-ukraine-app
 // load the things we need
 
+require('dotenv').config();
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -9,42 +10,65 @@ const connectionString = process.env.DATABASE_URL;
 const { Pool } = require('pg')
 const pool = new Pool({connectionString: connectionString});
 
-// block of code to test to query data from the person table
-var sql = "SELECT * FROM placesofinterest";
-
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
-
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result.rows);
-
-
-});
 
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   // index2 page
-  .get('/', (req, res) => pool.query('SELECT * FROM placesofinterest', function(err, result) {
-	  console.log(result);
-	  //var placesOfInterest = result.rows;
+  .get('/', (req, res) => res.render('pages/index2'))
+  .get('/placesofinterestlist', getPlacesOfInterest)
+	
+  // placesofinterest page
+  .get('/placesofinterest', (req, res) => res.render('pages/placesofinterest'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  
+  function getPlacesOfInterest(req, res){
+	  console.log("Getting places of interest from DB");
+	
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+  pool.query('SELECT * FROM placesofinterest', function(err, result) {
       if (err) {
         return console.error('error running query', err);
       }
 	  
 	  // Log this to the console for debugging purposes.
     console.log("Back from DB with result:");
-	console.log(result);
-    })
-	//res.render('pages/index2')
-	)
+	console.log(result.rows);
+	const placesOfInterest = result.rows;
+	console.log("placesOfInterest variable:");
+	console.log(placesOfInterest);
+	//res.status(200).json(placesOfInterest);
 	
-  // placesofinterest page
-  .get('/placesofinterest', (req, res) => res.render('pages/placesofinterest'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	res.render('pages/placesofinterestlist', {
+        placesOfInterest: placesOfInterest
+    });
+	
+	//callback(null, result.rows);
+    });
+	  
+	  /*
+	  getPlacesOfInterestFromDb(function(error, result){
+		
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			res.status(500).json({success: false, data: error});
+		} else {
+			const placesOfInterest = result;
+			//res.status(200).json(placesOfInterest);
+			res.status(200).json(placesOfInterest);
+			//res.render('pages/index2', {
+        //placesOfInterest: placesOfInterest
+    //});
+		}
+	});
+	*/ 
+	  	
+  }
+  
+  
+  function getPlacesOfInterestFromDb(callback) {
+	
+  }
