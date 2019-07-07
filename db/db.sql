85e358d25b7dec1698dc366a9addefdbc6b4b5dd
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-07-07 06:19:24 +0300

    add

--- db/db.sql
+++ db/db.sql
@@ -17,11 +17,36 @@ locationname VARCHAR(100) NOT NULL,
 population INT NOT NULL
 );
 
+CREATE TABLE contacts (
+id SERIAL PRIMARY KEY,
+contactname VARCHAR(100) NOT NULL,
+contactemail VARCHAR(100) NOT NULL,
+message VARCHAR(700) NOT NULL
+);
+
+INSERT INTO contacts (contactname, contactemail, message) VALUES ('Kiev', 2900000);
+
+
 INSERT INTO locations (locationname, population) VALUES ('Kiev', 2900000);
 
 DROP TABLE placesofinterest;
 
 INSERT INTO placesofinterest (placeofinterestname, placeofinterestdescription, locationname, locationid, priceforvisit, locationmap, openhours, phonenumber, website) VALUES ('Kiev Pechersk Lavra', 'Kiev Pechersk Lavra, also known as the Kiev Monastery of the Caves, is a historic Orthodox Christian monastery. Since its foundation as the cave monastery in 1051 the Lavra has been a preeminent center of Eastern Orthodox Christianity in Eastern Europe. Together with the Saint Sophia Cathedral, it is inscribed as a UNESCO World Heritage Site.', 'Kiev', 1, 'part of Lavra is free for access, another part of Lavra is accessble only for a payment in the amount of ', 'https://www.kplavra.kiev.ua/en/locationmap', 'daily from 9:00 am to 7:00 pm', 'Tours Desk: +38 (044) 406 63 75', 'https://www.kplavra.kiev.ua/en');
 INSERT INTO placesofinterest (placeofinterestname, placeofinterestdescription, locationname, locationid, priceforvisit, locationmap, openhours, phonenumber, website) VALUES ('Saint Sophia Cathedral', 'An outstanding architectural monument of Kievan Rus. The cathedral is one of the best known landmarks of the city and the first heritage site in Ukraine to be inscribed on the World Heritage List along with the Kiev Cave Monastery complex. The first foundations were laid in 1037 or 1011, but the cathedral took two decades to complete.', 'Kiev', 1, 'https://st-sophia.org.ua/en/news/tickets-prices/', 'locationmap', 'Opening Hours: 10:00 - 18:00, Saturday: 10:00 - 17:00 Closed on Thursday', '+38 (044)278-26-20', 'https://st-sophia.org.ua/en/home/');
+INSERT INTO placesofinterest (placeofinterestname, placeofinterestdescription, locationname, locationid, priceforvisit, locationmap, openhours, phonenumber, website, image) VALUES ('Khortytsya National Park', 'The major part of the reserve (historic park) covers the Zaporizhian Cossack Museum that includes the Cossack horse show. The museum building is modern, nestling low in the landscape with dramatic views of the Dnieper Hydroelectric Station to the north. The expo area of the museum was accounted for 1,600 m2 (17,000 sq ft) portraying the following themes: Khortytsia in ancient times, history of Zaporizhian Cossacks, history of Zaporizhia at times of construction of socialism. There also existed four dioramas: "Battle of Sviatoslav at rapids" (author - M.Oviechkin), "Uprising of the impoverished cossacks at Zaporizhian Sich in 1768" (M.Oviechkin), "Construction of Dnieper HES" (V.Trotsenko), "Night storm of Zaporizhia city in October 1943" (M.Oviechkin). The museum contains exhibits dating from the Stone Age through the Scythian period (c.750 - 250 BC) down to the 20th century.', 'Zaporizhia', 1, '48 UAH', 'https://en.wikipedia.org/wiki/Khortytsia', 'From 9:30 am to 16:30 pm', 'Tours Desk: +38 (096)-254-12-09, +38 (095)-914-77-06', 'https://en.wikipedia.org/wiki/Khortytsia', 'images/khortitza_sich.jpg');
 
 
+ALTER TABLE placesofinterest
+ADD image varchar(150);
+
+ALTER TABLE placesofinterest
+ALTER COLUMN placeofinterestdescription TYPE varchar(950);
+
+
+UPDATE placesofinterest
+SET image = 'images/kiev_lavra.jpg'
+WHERE id = 2;
+
+UPDATE placesofinterest
+SET image = 'images/sofia-kievskaya3.jpg'
+WHERE id = 3;
--- index.js
+++ index.js
@@ -8,21 +8,49 @@ const PORT = process.env.PORT || 5000
 const app = express();
 const connectionString = process.env.DATABASE_URL;
 const { Pool } = require('pg')
+var myParser = require("body-parser");
 const pool = new Pool({connectionString: connectionString});
 
 
 app
   .use(express.static(path.join(__dirname, 'public')))
+  .use(myParser.urlencoded({extended : true}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   // index2 page
   .get('/', (req, res) => res.render('pages/index2'))
+  .get('/getcontactform', (req, res) => res.render('pages/contactform'))
+  .post('/contactinfo', saveContactInfo)
   .get('/placesofinterestlist', getPlacesOfInterest)
 	
   // placesofinterest page
-  .get('/placesofinterest', (req, res) => res.render('pages/placesofinterest'))
+  //.get('/placesofinterest', (req, res) => res.render('pages/placesofinterest'))
+  .get('/placesofinterest', getPlacesOfInterest)
+  .get('/placeofinterestdetails', getPlaceOfInterestDetails)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
+  function saveContactInfo(req, res){
+	  console.log("Inserting info into DB");
+	console.log(req.body);
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+  pool.query('INSERT INTO contacts (contactname, contactemail, message) VALUES ($1, $2, $3)', [req.body.fname, req.body.email, req.body.message], function(err, result) {
+	  
+      if (err) {
+        return console.error('error running query', err);
+      }
+	  
+	  // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+	console.log(result);
+	console.log(`User added with ID: ${result.insertId}`);
+	//res.status(200).json(placesOfInterest);
+	
+	res.render('pages/answer');
+	
+	//callback(null, result.rows);
+    });
+  }
   
   function getPlacesOfInterest(req, res){
 	  console.log("Getting places of interest from DB");
@@ -48,23 +76,37 @@ app
 	
 	//callback(null, result.rows);
     });
+  }
+	
+	
+	
+	function getPlaceOfInterestDetails(req, res){
+	  console.log("Getting place of interest from DB");
+	
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+  pool.query('SELECT * FROM placesofinterest WHERE id=' + req.query.id + '', function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
 	  
-	  /*
-	  getPlacesOfInterestFromDb(function(error, result){
-		
-		// Make sure we got a row with the person, then prepare JSON to send back
-		if (error || result == null || result.length != 1) {
-			res.status(500).json({success: false, data: error});
-		} else {
-			const placesOfInterest = result;
-			//res.status(200).json(placesOfInterest);
-			res.status(200).json(placesOfInterest);
-			//res.render('pages/index2', {
-        //placesOfInterest: placesOfInterest
-    //});
-		}
-	});
-	*/ 
+	  // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+	console.log(req.query.id);
+	console.log(result.rows);
+	const placeOfInterest = result.rows;
+	console.log("placeOfInterest variable:");
+	console.log(placeOfInterest);
+	console.log(placeOfInterest[0].placeofinterestname);
+	
+	//res.status(200).json(placesOfInterest);
+	
+	res.render('pages/placeofinterest', {
+        placeOfInterest: placeOfInterest[0]
+    });
+	
+	//callback(null, result.rows);
+    });
 	  	
   }
   
@@ -72,3 +114,5 @@ app
   function getPlacesOfInterestFromDb(callback) {
 	
   }
+  
+  // cd cs313-node/travel_in_ukraine_app
